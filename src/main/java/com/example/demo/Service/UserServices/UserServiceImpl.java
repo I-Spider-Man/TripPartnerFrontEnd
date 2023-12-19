cdc7983fe5469e0067feb7fcbbf4d16d33ce1f6b
I-Spider-Man <einstein26092001@gmail.com>
2023-12-19 17:46:06 +0530

    Revert "email for user login"
    This reverts commit aad90694101b920f2ead716ae2b3dc29ff3575da.

---
+++ src/main/java/com/example/demo/Service/UserServices/UserServiceImpl.java
@@ -0,0 +1,60 @@
+package com.example.demo.Service.UserServices;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.User;
+import com.example.demo.Repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserRepository userRepo;
+	@Override
+	public List<User> getAllUser() {
+		return (List<User>) userRepo.findAll(); 
+	}
+	@Override
+	public User getUserById(Integer userId) {
+		return userRepo.findById(userId).get();
+	}
+	
+	@Override
+	public boolean addUser(User user) {
+		if(checkUser(user.getUserEmail())) {
+			return false;
+		}
+		else {
+			userRepo.save(user);
+			return true;
+		}
+	}
+
+	@Override
+	public String removeUserById(Integer userId) {
+		Optional<User> user=userRepo.findById(userId);
+		if(user.isPresent()){
+			userRepo.deleteById(userId);
+			return "user with id: "+userId+" is removed successfully";
+		}
+		else {
+			return "user with id: "+userId+" is not found";
+		}
+	}
+	public boolean checkUser(String userEmail) {
+        return userRepo.findByUserEmail(userEmail).isPresent();
+	}
+	@Override
+	public User getByUserEmail(String userEmail) {
+		Optional<User> user=userRepo.findByUserEmail(userEmail);
+		return user.orElse(null);
+	}
+	@Override
+	public List<User> getAllByUserName(String userName) {
+		return userRepo.findAllByUserName(userName);
+	}
+
+}
