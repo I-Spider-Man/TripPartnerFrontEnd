5227af883da37f8a98ee0e6263786b9e092fe4a5
I-Spider-Man <einstein26092001@gmail.com>
2023-12-19 17:25:16 +0530

    Revert "email for user login"
    This reverts commit aad90694101b920f2ead716ae2b3dc29ff3575da.

--- src/main/java/com/example/demo/Repository/EventRepository.java
+++ src/main/java/com/example/demo/Repository/EventRepository.java
@@ -3,10 +3,9 @@ package com.example.demo.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.Model.Event;
-@Repository
+
 public interface EventRepository extends JpaRepository<Event, Integer>{
 	Optional<Event> findByEventName(String eventName);
 }
