c2bca3aae834de3700c3c3ccd6df1539c860d013
Bhavyasreet29 <bhavyat29@gmail.com>
2024-01-31 09:57:09 +0530

    d

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
         <Route path='/Spot/:spotName' element={<TouristSpot/>}/>
         <Route path="/component" element={<BlogSlider/>}/>
         <Route path='/profile/:userId' element={<ProfilePage/>}/>
+        <Route path='/GroupPage' element={<GroupPage/>}/>
       </Routes></div>
       <div className='footer'><Footer/></div>
     </div>
