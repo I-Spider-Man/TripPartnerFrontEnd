564476e30e92305826bca179d5bdf51fd42accd1
Bhavyasreet29 <bhavyat29@gmail.com>
2024-02-01 15:21:37 +0530

    join/leave button

--- src/Components/GroupPage/GroupPage.js
+++ src/Components/GroupPage/GroupPage.js
@@ -8,12 +8,14 @@ import { Organizer, participants } from './OrganizerData';
 import { useParams } from 'react-router-dom';
 import { getAllParticipantsById, getGroupById } from '../Files/Group_Details';
 import { fetchOrganizerDataById } from '../Files/Organzier_Details';
+import { participantJoining } from '../Files/Participant_Details';
 
 const GroupPage = () => {
 const [groupDetails,setGroupDetails]=useState(null);
 const [organizerData,setOrganizerData]=useState(null);
 const [participantData,setParticipantData]=useState(null);
 const {groupId}=useParams();
+
 useEffect(()=>{
   console.log("renders");
   const fetchData=async()=>{
@@ -51,6 +53,17 @@ useEffect(()=>{
   fetchParticipant();
 },[groupDetails]);
 console.log(groupDetails,participantData,organizerData);
+
+const handleJoinClick = () => {
+  alert('Join button clicked!');
+  // Add your leave logic here
+};
+const handleLeaveClick = () => {
+  alert('Leave button clicked!');
+  // Add your leave logic here
+};
+
+
   return (
     groupDetails && participantData && organizerData && (
     <div className='body1'>
@@ -59,6 +72,18 @@ console.log(groupDetails,participantData,organizerData);
         <h1 className="headerh1">
           <span className="group-icon">👥</span> {groupDetails.groupName}
         </h1>
+        <div className='button-32'>
+          
+        <button onClick={handleJoinClick}>
+             Join
+        </button>
+        </div>
+
+      <div className='button-32'>
+        <button onClick={handleLeaveClick}>
+             Leave
+        </button>
+        </div>
       </div>
       <div className="organizer-info">
         <img src={organizerData.userData.userProfile} alt="Organizer Profile" className="profile-pic" />
