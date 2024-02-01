6c5ddd4ed19c15f5fc1c0ef3853e4d5b64bd1b0e
Bhavyasreet29 <bhavyat29@gmail.com>
2024-02-01 17:47:56 +0530

    join/leave changes

--- src/Components/GroupPage/GroupPage.js
+++ src/Components/GroupPage/GroupPage.js
@@ -1,21 +1,21 @@
 // GroupPage.js
 import React, { useEffect, useState } from 'react';
-// Correct import statement
-import ParticipantList from './ParticipantList';
 import './GroupPage.css';
-
-import { Organizer, participants } from './OrganizerData';
 import { useParams } from 'react-router-dom';
 import { getAllParticipantsById, getGroupById } from '../Files/Group_Details';
 import { fetchOrganizerDataById } from '../Files/Organzier_Details';
 import { participantJoining } from '../Files/Participant_Details';
+import { useUser } from '../Auth/UserContext';
+import ParticipantList from './ParticipantList';
 
 const GroupPage = () => {
+  const {organizerData,participantData}=useUser();
 const [groupDetails,setGroupDetails]=useState(null);
-const [organizerData,setOrganizerData]=useState(null);
-const [participantData,setParticipantData]=useState(null);
+const [organizer,setOrganizer]=useState(null);
+const [participants,setParticipants]=useState(null);
 const {groupId}=useParams();
-
+console.log(organizerData,groupId);
+console.log(organizerData.groupId===groupId);
 useEffect(()=>{
   console.log("renders");
   const fetchData=async()=>{
@@ -34,7 +34,7 @@ useEffect(()=>{
   const fetchOrganizer=async()=>{
     try{
       const response=await fetchOrganizerDataById(groupDetails.organizerId);
-      setOrganizerData(response);
+      setOrganizer(response);
     }catch(error){
       console.log(error);
     }
@@ -45,14 +45,14 @@ useEffect(()=>{
   const fetchParticipant=async()=>{
     try{
       const response=await getAllParticipantsById(groupDetails.groupId);
-      setParticipantData(response);
+      setParticipants(response);
     }catch(error){
       console.log(error);
     }
   }
   fetchParticipant();
 },[groupDetails]);
-console.log(groupDetails,participantData,organizerData);
+console.log(groupDetails,participants,organizer);
 
 const handleJoinClick = () => {
   alert('Join button clicked!');
@@ -65,37 +65,48 @@ const handleLeaveClick = () => {
 
 
   return (
-    groupDetails && participantData && organizerData && (
+    groupDetails && participants && organizer && (
     <div className='body1'>
         <div className="group-container">
       <div className="header1">
         <h1 className="headerh1">
           <span className="group-icon">👥</span> {groupDetails.groupName}
         </h1>
-        <div className='button-32'>
-          
-        <button onClick={handleJoinClick}>
-             Join
-        </button>
-        </div>
+        {!(organizerData.groupId== groupId || participantData.groupId== groupId )? (
+              <div className='button-32'>
+                <button onClick={handleJoinClick}>Join</button>
+              </div>
+            ) : null
+            }
+        {!(organizerData.groupId== groupId) ? (
+              <div className='button-32'>
+                <button onClick={handleLeaveClick}>Leave</button>
+              </div>
+            ) : null}    
+       
+      {
+           ( participantData.groupId === groupId) && (
+                <div className='button-32'>
+                    <button onClick={handleLeaveClick}>Leave</button>
+                </div>
+            )
+        }
+         
+      
 
-      <div className='button-32'>
-        <button onClick={handleLeaveClick}>
-             Leave
-        </button>
-        </div>
+            
       </div>
       <div className="organizer-info">
-        <img src={organizerData.userData.userProfile} alt="Organizer Profile" className="profile-pic" />
+        <img src={organizer.userData.userProfile} alt="Organizer Profile" className="profile-pic" />
         <div>
-          <h2 className="organizer-text">{organizerData.userData.userName}</h2>
+          <h2 className="organizer-text">{organizer.userData.userName}</h2>
         </div>
       </div>
       <div className='date-format'>
         <marquee><p>Date From: {groupDetails.dateFrom} Date To: {groupDetails.dateTo}</p></marquee>
       </div>
       <div id="participants-list">
-        {participantData.length > 0 ? <ParticipantList participants={participants} />:(<>no participants</>)}
+        {participants.length > 0 ? <ParticipantList participants={participants} />:(<>no participants</>)}
       </div>
     </div>
     </div>
