1d2af484c0891e08724444f8e859a10dc15d4ed6
Bhavyasreet29 <bhavyat29@gmail.com>
2024-02-13 15:05:35 +0530

    touridtspot

--- src/Components/Files/TouristSpotDetails.js
+++ src/Components/Files/TouristSpotDetails.js
@@ -1,84 +1,93 @@
 import { type } from "@testing-library/user-event/dist/type";
-import axios from "axios"
+import axios from "axios";
 export const pictureUrl = (image) => {
-    return `data:image/jpeg;base64,${image}`;
-  };
-export const fetchSpotBySpotName=async(spotName)=>{
-    try{
-        const response=await axios.get(`http://localhost:8080/spot/${spotName}`)
-        const picture=await axios.get(`http://localhost:8080/spot/pictureList/${response.data.spotId}`);
-        const imageList=picture.data.map(pic=>{
-            return pictureUrl(pic);
-          })
+  return `data:image/jpeg;base64,${image}`;
+};
+export const fetchSpotBySpotName = async (spotName) => {
+  try {
+    console.log(spotName);
+    const response = await axios.get(`http://localhost:8080/spot/${spotName}`);
+    console.log(response);
+    const picture = await axios.get(
+      `http://localhost:8080/spot/pictureList/${response.data.spotId}`
+    );
+    const imageList = picture.data.map((pic) => {
+      return pictureUrl(pic);
+    });
+    return {
+      ...response.data,
+      spotPictureList: imageList,
+    };
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+export const fetch_spot_data = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/spots");
+    const spotwithpicture = await Promise.all(
+      response.data.map(async (spot) => {
+        const picture = await axios.get(
+          `http://localhost:8080/spot/pictureList/${spot.spotId}`
+        );
+        const imageList = picture.data.map((pic) => {
+          return pictureUrl(pic);
+        });
         return {
-            ...response.data,
-            spotPictureList:imageList,
-        }
-    }catch(error){
-        console.log(error);
-        return [];
-    }
-}
-export const fetch_spot_data=async()=>{
-    try{
-        const response=await axios.get("http://localhost:8080/spots");
-        const spotwithpicture=await Promise.all(
-            response.data.map(async(spot)=>{
-                const picture=await axios.get(`http://localhost:8080/spot/pictureList/${spot.spotId}`);
-                const imageList=picture.data.map(pic=>{
-                    return pictureUrl(pic);
-                  })
-                return {
-                    ...spot,
-                    spotPictureList:imageList,
-                };
-            })
-        )
-        console.log(spotwithpicture)
-        return spotwithpicture;
-    }catch(error){
-        console.log("error while fetching spot data ",error);
-        return [];
-    }
-}
-
-export const fetch_popularSpots=async()=>{
-    try{
-        const response=await axios.get("http://localhost:8080/PopularSpots");
-        const spotwithpicture=await Promise.all(
-            response.data.map(async(spot)=>{
-                const picture=await axios.get(`http://localhost:8080/spot/pictureList/${spot.spotId}`);
-                const imageList=picture.data.map(pic=>{
-                    return pictureUrl(pic);
-                  })
-                return {
-                    ...spot,
-                    spotPictureList:imageList
-                };
-            })
-        )
-
-        return spotwithpicture;
-    }
-    catch(error){
-        console.log("error while fetching popular spots",error);
-        return [];
-    }
-}
-
-export const fetch_spots_by_id=async(id)=>{
-    try{
-        const response=await axios.get(`http://localhost:8080/spots/${id}`);
-        const picture=await axios.get(`http://localhost:8080/spot/pictureList/${response.data.spotId}`);
-        const imageList=picture.data.map(pic=>{
-            return pictureUrl(pic);
-          })
-        return {
-            ...response.data,
-            spotPictureList:imageList,
+          ...spot,
+          spotPictureList: imageList,
         };
-    }catch(error){
-        console.log("error while fetching spot by id",error);
-        return [];
-    }
-}
+      })
+    );
+    console.log(spotwithpicture);
+    return spotwithpicture;
+  } catch (error) {
+    console.log("error while fetching spot data ", error);
+    return [];
+  }
+};
+
+export const fetch_popularSpots = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/PopularSpots");
+    const spotwithpicture = await Promise.all(
+      response.data.map(async (spot) => {
+        const picture = await axios.get(
+          `http://localhost:8080/spot/pictureList/${spot.spotId}`
+        );
+        const imageList = picture.data.map((pic) => {
+          return pictureUrl(pic);
+        });
+        return {
+          ...spot,
+          spotPictureList: imageList,
+        };
+      })
+    );
+
+    return spotwithpicture;
+  } catch (error) {
+    console.log("error while fetching popular spots", error);
+    return [];
+  }
+};
+
+export const fetch_spots_by_id = async (id) => {
+  try {
+    const response = await axios.get(`http://localhost:8080/spots/${id}`);
+    const picture = await axios.get(
+      `http://localhost:8080/spot/pictureList/${response.data.spotId}`
+    );
+    const imageList = picture.data.map((pic) => {
+      return pictureUrl(pic);
+    });
+    return {
+      ...response.data,
+      spotPictureList: imageList,
+    };
+  } catch (error) {
+    console.log("error while fetching spot by id", error);
+    return [];
+  }
+};
